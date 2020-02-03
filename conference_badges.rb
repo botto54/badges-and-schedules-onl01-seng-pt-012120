9ee779aa151008f575b9779b27b1269d81ade027
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-03 04:20:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |speakers|
+    "Hello, my name is #{speakers}."
+  end
+end
+
+def assign_rooms(attendees)
+end
+
+def printer(attendees)
+end
